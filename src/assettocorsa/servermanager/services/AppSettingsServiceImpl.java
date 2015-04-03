2e0471f1e002ad6b4d8c7c6a46c4636c87f68a13
p3t3c <petec33@gmail.com>
2015-04-03 15:18:07 +1100

    Better name for App settings data class

--- src/assettocorsa/servermanager/services/AppSettingsServiceImpl.java
+++ src/assettocorsa/servermanager/services/AppSettingsServiceImpl.java
@@ -1,6 +1,6 @@
 package assettocorsa.servermanager.services;
 
-import assettocorsa.servermanager.services.data.AppSettings;
+import assettocorsa.servermanager.services.data.AppSettingsData;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
@@ -13,12 +13,12 @@ public class AppSettingsServiceImpl implements IAppSettingsService {
     public static final String ASSETTO_CORSA_DEFAULT_DIR = "C:\\Program Files (x86)\\Steam\\SteamApps\\common\\assettocorsa";
     private static final String DEFAULT_EXPORT_DIR = System.getProperty("user.home" + File.separator+ "Documents");
     private final String applicationSettingsDirectory;
-    private AppSettings settings;
+    private AppSettingsData settings;
 
     public AppSettingsServiceImpl() {
         /* This call gets the location where the program is run from. Will use this to store the settings */
         applicationSettingsDirectory = this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath();
-        settings = new AppSettings();
+        settings = new AppSettingsData();
     }
 
     @Override
@@ -27,7 +27,7 @@ public class AppSettingsServiceImpl implements IAppSettingsService {
         if (settingsFile.exists() && settingsFile.canRead()) {
             Reader reader = new FileReader(settingsFile);
             Gson gson = new GsonBuilder().create();
-            settings = gson.fromJson(reader, AppSettings.class);
+            settings = gson.fromJson(reader, AppSettingsData.class);
             reader.close();
         } else {
             loadDefaultAppSettings();
--- src/assettocorsa/servermanager/services/data/AppSettingsData.java
+++ src/assettocorsa/servermanager/services/data/AppSettingsData.java
@@ -4,7 +4,7 @@ package assettocorsa.servermanager.services.data;
  * Created by pete on 3/04/2015.
  * Holds various settings for the server manager application itself.
  */
-public class AppSettings {
+public class AppSettingsData {
     private String assettoCorsaDirectory;
     private String exportDirectory;
 
