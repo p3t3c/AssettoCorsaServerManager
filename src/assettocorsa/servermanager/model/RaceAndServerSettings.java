2371b8608b3a0bbf6ff27e30dc37f9a3d888e0c5
p3t3c <petec33@gmail.com>
2015-04-05 16:04:57 +1000

    Draft version

---
+++ src/assettocorsa/servermanager/model/RaceAndServerSettings.java
@@ -0,0 +1,42 @@
+package assettocorsa.servermanager.model;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.property.StringProperty;
+
+/**
+ * Created by pete on 4/04/2015.
+ */
+public class RaceAndServerSettings {
+
+    /**
+     * Name to be displayed on a list of races.
+     */
+    private StringProperty raceName;
+
+    /**
+     * Server configuration, the name of the server
+     */
+    private StringProperty serverName;
+
+    RaceAndServerSettings() {
+        raceName = new SimpleStringProperty();
+        serverName = new SimpleStringProperty();
+    }
+
+    public String getRaceName() {
+        return raceName.get();
+    }
+
+    public StringProperty raceNameProperty() {
+        return raceName;
+    }
+
+    public String getServerName() {
+        return serverName.get();
+    }
+
+    public StringProperty serverNameProperty() {
+        return serverName;
+    }
+
+}
---
+++ src/assettocorsa/servermanager/model/RaceUIModel.java
@@ -0,0 +1,10 @@
+package assettocorsa.servermanager.model;
+
+import javafx.collections.ObservableList;
+
+/**
+ * Created by pete on 4/04/2015.
+ */
+public interface RaceUIModel {
+    //public ObservableList<RaceAndServerSettings>
+}
