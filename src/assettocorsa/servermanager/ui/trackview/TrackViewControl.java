de06f926d222055f1394f66d5822336308b386f5
p3t3c <petec33@gmail.com>
2015-04-13 22:04:52 +1000

    Simple implementation of the TrackViewControl

--- src/assettocorsa/servermanager/ui/trackview/TrackViewControl.java
+++ src/assettocorsa/servermanager/ui/trackview/TrackViewControl.java
@@ -3,17 +3,24 @@ package assettocorsa.servermanager.ui.trackview;
 import com.github.benjamingale.usercontrol.UserControl;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.Region;
+import javafx.scene.layout.VBox;
+
+import java.io.IOException;
 
 /**
  * Created by pete on 8/04/2015.
  */
 public class TrackViewControl extends UserControl {
-    public Label trackName;
+
     public ImageView trackImage;
     public Label trackPitboxes;
+    public Label trackName;
 
     public StringProperty trackNameProperty() {
         return trackName.textProperty();
@@ -24,6 +31,6 @@ public class TrackViewControl extends UserControl {
     }
 
     public StringProperty trackPitboxesProperty() {
-        return trackPitboxesProperty();
+        return trackPitboxes.textProperty();
     }
 }
