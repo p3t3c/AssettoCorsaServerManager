b9e720568e93d2d08f35a46a6f7b0bd47fea5e52
p3t3c <petec33@gmail.com>
2015-04-16 18:49:16 +1000

    trackModelList (ObseravleList) now fires changes on change to selectedProperty of TrackModel. This allows the newly added changeListener in this class to manage the selected flags of TrackModels to ensure that only a single selection is made.

--- src/assettocorsa/servermanager/ui/trackview/TrackViewControl.java
+++ src/assettocorsa/servermanager/ui/trackview/TrackViewControl.java
@@ -4,15 +4,14 @@ import assettocorsa.servermanager.model.TrackModel;
 import com.github.benjamingale.usercontrol.UserControl;
 import javafx.beans.property.ObjectProperty;
 import javafx.beans.property.StringProperty;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.event.Event;
 import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.Region;
-import javafx.scene.layout.VBox;
-
-import java.io.IOException;
+import javafx.scene.paint.Color;
 
 /**
  * Created by pete on 8/04/2015.
@@ -29,10 +28,7 @@ public class TrackViewControl extends UserControl {
         this.trackNameProperty().bind(trackModel.trackNameProperty());
         this.trackImageProperty().bind(trackModel.trackImageProperty());
         this.trackPitboxesProperty().bind(trackModel.trackPitboxesProperty());
-    }
-
-    public StringProperty trackNameProperty() {
-        return trackName.textProperty();
+        this.trackModel.selectedProperty().addListener(createSelectionListener());
     }
 
     public ObjectProperty<Image> trackImageProperty() {
@@ -43,7 +39,44 @@ public class TrackViewControl extends UserControl {
         return trackPitboxes.textProperty();
     }
 
+    public StringProperty trackNameProperty() {
+        return trackName.textProperty();
+    }
+
     public TrackModel getTrackModel() {
         return trackModel;
+    }
+
+    /**
+     * When the mouse is clicked on any of the views Nodes
+     *
+     * @param actionEvent
+     */
+    public void mouseClickOnTrack(Event actionEvent) {
+        System.out.println("Clicked on " + trackNameProperty().getValue()); // TODO proper logger
+        if (!trackModel.selectedProperty().get()) { // if not already selected
+            trackModel.selectedProperty().setValue(true);
+        }
+    }
+
+    private ChangeListener<? super Boolean> createSelectionListener() {
+        return new ChangeListener<Boolean>() {
+            @Override
+            public void changed(ObservableValue<? extends Boolean> observable, Boolean oldValue, Boolean newValue) {
+                if (newValue) {
+                    final int depth = 70;
+                    DropShadow borderGlow = new DropShadow();
+                    borderGlow.setOffsetY(0f);
+                    borderGlow.setOffsetX(0f);
+                    borderGlow.setColor(Color.BLUE);
+                    borderGlow.setWidth(depth);
+                    borderGlow.setHeight(depth);
+
+                    setEffect(borderGlow);
+                } else {
+                    setEffect(null);
+                }
+            }
+        };
     }
 }
